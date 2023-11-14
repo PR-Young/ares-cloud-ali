8f33d798a960bc42ef354578a80732942c616afd
Young <1065569578@qq.com>
2023-11-14 16:47:11 +0800

    20231114|修改打包配置

--- ares-modules/ares-system/src/test/java/com/ares/AresApplicationTests.java
+++ ares-modules/ares-system/src/test/java/com/ares/AresApplicationTests.java
@@ -21,11 +21,12 @@
 package com.ares;
 
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AresApplicationTests {
+
 
     @Test
     void contextLoads() {
